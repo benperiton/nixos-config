5bd5cc6eefbd0237e3c855b2e4bfd51d0e24d01b
Ben Periton <dev@ben.periton.co.uk>
2023-05-30 14:01:20 +0100

    Test

--- hosts/_common/global/default.nix
+++ hosts/_common/global/default.nix
@@ -21,15 +21,6 @@
 
   services.openssh = {
     enable = true;
-    settings = {
-      # Harden
-      #PasswordAuthentication = false;
-      PermitRootLogin = "no";
-      # Automatically remove stale sockets
-      StreamLocalBindUnlink = "yes";
-      # Allow forwarding ports to everywhere
-      GatewayPorts = "clientspecified";
-    };
   };
 
   # Increase open file limit for sudoers
