9cb799b3709c25a46a400b808f5686bbf04e15bf
Ben Periton <dev@ben.periton.co.uk>
2023-05-30 13:59:09 +0100

    Test

--- hosts/vbox/default.nix
+++ hosts/vbox/default.nix
@@ -1,1 +1,19 @@
-default.nix
+{ pkgs, inputs, ... }: {
+  imports = [
+    ./hardware-configuration.nix
+
+    ../_common/global
+  ];
+
+  networking = {
+    hostName = "vbox";
+    useDHCP = true;
+  };
+
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+    binfmt.emulatedSystems = [ "aarch64-linux" "i686-linux" ];
+  };
+  
+  system.stateVersion = "22.05";
+}
